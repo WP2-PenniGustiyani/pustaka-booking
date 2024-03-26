dc47bf99eace6b2420ff936ed4c879b4a5ca98f1
WP2-PenniGustiyani <pennigustiyani488@gmail.com>
2024-03-26 20:24:13 +0700

    membuar pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Peni Gustiyani 
+            Saya tinggal di daerah Depok 
+            olahraga yang saya sukai adalah 
+            bulutangkis ";
+    }
+}
