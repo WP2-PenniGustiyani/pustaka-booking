d1ebf288d4aeaf959da79a390009b190aae19ab9
WP2-PenniGustiyani <pennigustiyani488@gmail.com>
2024-06-16 15:22:31 +0700

    pertemuan 12

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,7 +7,7 @@ class Admin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        // cek_login();
+        cek_login();
     }
 
     public function index()
